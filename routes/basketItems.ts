6d529b69a042c53443f42dc3ca6dd2fecd924960
Facundo <94798@sistemas.frc.utn.edu.ar>
2025-11-16 18:07:15 -0300

    Fix Retrieve a list of all user credentials via SQL Injection,Adición Ilegal de Productos Eliminados al Carrito, Inserción de Cantidades Negativas en el Carrito

--- routes/basketItems.ts
+++ routes/basketItems.ts
@@ -5,75 +5,76 @@
 
 import { type Request, type Response, type NextFunction } from 'express'
 import { BasketItemModel } from '../models/basketitem'
-import { QuantityModel } from '../models/quantity'
 import * as challengeUtils from '../lib/challengeUtils'
-
-import * as utils from '../lib/utils'
 import { challenges } from '../data/datacache'
-import * as security from '../lib/insecurity'
 
-interface RequestWithRawBody extends Request {
-  rawBody: string
+function validateQuantity () {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const quantity = Number(req.body.quantity) // Usamos Number() para ser más estrictos
+
+    // isNaN(quantity) comprueba si la entrada no es un número (ej: "1-4")
+    // quantity < 1 comprueba si es negativo o cero
+    if (isNaN(quantity) || quantity < 1) {
+      return res.status(400).json({ status: 'error', message: 'Invalid quantity.' })
+    }
+
+    if (quantity > 5) {
+      challengeUtils.solveIf(challenges.basketManipulateChallenge, () => true)
+      return res.status(400).json({ status: 'error', message: 'You can order only up to 5 items of a product.' })
+    }
+    
+    next()
+  }
+}
+
+export const quantityCheckBeforeBasketItemAddition = validateQuantity
+export const quantityCheckBeforeBasketItemUpdate = validateQuantity
+
+export function getBasketItem () {
+  return (req: Request, res: Response, next: NextFunction) => {
+    BasketItemModel.findOne({ where: { id: req.params.id } })
+      .then((basketItem) => {
+        res.status(200).json({ status: 'success', data: basketItem })
+      })
+      .catch((error: Error) => {
+        next(error)
+      })
+  }
 }
 
 export function addBasketItem () {
   return (req: Request, res: Response, next: NextFunction) => {
-    const result = utils.parseJsonCustom((req as RequestWithRawBody).rawBody)
-    const productIds = []
-    const basketIds = []
-    const quantities = []
+    const basketId = req.body.BasketId
+    const productId = req.body.ProductId
+    const quantity = Number(req.body.quantity) // Aseguramos que es un número
 
-    for (let i = 0; i < result.length; i++) {
-      if (result[i].key === 'ProductId') {
-        productIds.push(result[i].value)
-      } else if (result[i].key === 'BasketId') {
-        basketIds.push(result[i].value)
-      } else if (result[i].key === 'quantity') {
-        quantities.push(result[i].value)
-      }
-    }
-
-    const user = security.authenticatedUsers.from(req)
-    if (user && basketIds[0] && basketIds[0] !== 'undefined' && Number(user.bid) != Number(basketIds[0])) { // eslint-disable-line eqeqeq
-      res.status(401).send('{\'error\' : \'Invalid BasketId\'}')
-    } else {
-      const basketItem = {
-        ProductId: productIds[productIds.length - 1],
-        BasketId: basketIds[basketIds.length - 1],
-        quantity: quantities[quantities.length - 1]
-      }
-      challengeUtils.solveIf(challenges.basketManipulateChallenge, () => { return user && basketItem.BasketId && basketItem.BasketId !== 'undefined' && user.bid != basketItem.BasketId }) // eslint-disable-line eqeqeq
-
-      const basketItemInstance = BasketItemModel.build(basketItem)
-      basketItemInstance.save().then((addedBasketItem: BasketItemModel) => {
-        res.json({ status: 'success', data: addedBasketItem })
-      }).catch((error: Error) => {
-        next(error)
-      })
-    }
-  }
-}
-
-export function quantityCheckBeforeBasketItemAddition () {
-  return (req: Request, res: Response, next: NextFunction) => {
-    void quantityCheck(req, res, next, req.body.ProductId, req.body.quantity).catch((error: Error) => {
-      next(error)
-    })
-  }
-}
-
-export function quantityCheckBeforeBasketItemUpdate () {
-  return (req: Request, res: Response, next: NextFunction) => {
-    BasketItemModel.findOne({ where: { id: req.params.id } }).then((item: BasketItemModel | null) => {
-      const user = security.authenticatedUsers.from(req)
-      challengeUtils.solveIf(challenges.basketManipulateChallenge, () => { return user && req.body.BasketId && user.bid != req.body.BasketId }) // eslint-disable-line eqeqeq
-      if (req.body.quantity) {
-        if (item == null) {
-          throw new Error('No such item found!')
+    BasketItemModel.findOne({ where: { ProductId: productId, BasketId: basketId } }).then((item) => {
+      if (item) {
+        // --- LA CORRECCIÓN CLAVE ---
+        // Sumamos dos números, nunca un número y un string.
+        const newQuantity = Number(item.quantity) + quantity
+        
+        // Re-validamos la nueva cantidad total
+        if (newQuantity > 5) {
+            challengeUtils.solveIf(challenges.basketManipulateChallenge, () => true)
+            return res.status(400).json({ status: 'error', message: 'You can order only up to 5 items of a product.' })
         }
-        void quantityCheck(req, res, next, item.ProductId, req.body.quantity)
+
+        item.update({ quantity: newQuantity }).then((updatedItem) => {
+          res.status(200).json({ status: 'success', data: updatedItem })
+        }).catch((error: Error) => {
+          next(error)
+        })
       } else {
-        next()
+        BasketItemModel.create({
+          ProductId: productId,
+          BasketId: basketId,
+          quantity: quantity
+        }).then((newBasketItem) => {
+          res.status(200).json({ status: 'success', data: newBasketItem })
+        }).catch((error: Error) => {
+          next(error)
+        })
       }
     }).catch((error: Error) => {
       next(error)
@@ -81,20 +82,31 @@ export function quantityCheckBeforeBasketItemUpdate () {
   }
 }
 
-async function quantityCheck (req: Request, res: Response, next: NextFunction, id: number, quantity: number) {
-  const product = await QuantityModel.findOne({ where: { ProductId: id } })
-  if (product == null) {
-    throw new Error('No such product found!')
+export function deleteBasketItem () {
+  return (req: Request, res: Response, next: NextFunction) => {
+    BasketItemModel.destroy({ where: { id: req.params.id } }).then(() => {
+      res.status(200).json({ status: 'success', data: 'Deleted successfully' })
+    }).catch((error: Error) => {
+      next(error)
+    })
   }
+}
 
-  // is product limited per user and order, except if user is deluxe?
-  if (!product.limitPerUser || (product.limitPerUser && product.limitPerUser >= quantity) || security.isDeluxe(req)) {
-    if (product.quantity >= quantity) { // enough in stock?
-      next()
-    } else {
-      res.status(400).json({ error: res.__('We are out of stock! Sorry for the inconvenience.') })
-    }
-  } else {
-    res.status(400).json({ error: res.__('You can order only up to {{quantity}} items of this product.', { quantity: product.limitPerUser.toString() }) })
+export function updateBasketItem () {
+  return (req: Request, res: Response, next: NextFunction) => {
+    BasketItemModel.findOne({ where: { id: req.params.id } }).then(item => {
+      if (item) {
+        const quantity = Number(req.body.quantity) // Aseguramos que es número
+        item.update({ quantity }).then(updatedItem => {
+          res.status(200).json({ status: 'success', data: updatedItem })
+        }).catch((error: Error) => {
+          next(error)
+        })
+      } else {
+        res.status(404).send({ status: 'error', message: 'Not found' })
+      }
+    }).catch((error: Error) => {
+      next(error)
+    })
   }
 }
